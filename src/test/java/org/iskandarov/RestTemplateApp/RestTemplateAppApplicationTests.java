503c017b3c38d6a2201a56ee6568d57a02390556
Orkhan12 <322komputer322>
2021-03-17 14:45:26 +0300

    Initial commit

---
+++ src/test/java/org/iskandarov/RestTemplateApp/RestTemplateAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iskandarov.RestTemplateApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTemplateAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
